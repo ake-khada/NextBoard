9a51ac7bb7bba426b895c33426ec78717e092bd1
nextboard-agent <nextboard-agent@users.noreply.github.com>
2026-09-02 20:02:21 +0200

    Brand app as NextBoard

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,7 @@ android {
             }
             variant.outputs.forEach { output ->
                 if (output is com.android.build.api.variant.impl.VariantOutputImpl) {
-                    output.outputFileName = "HeliBoard_${defaultConfig.versionName}-${variant.buildType}.apk"
+                    output.outputFileName = "NextBoard_${defaultConfig.versionName}-${variant.buildType}.apk"
                 }
             }
         }
--- app/src/test/java/helium314/keyboard/Shadows.kt
+++ app/src/test/java/helium314/keyboard/Shadows.kt
@@ -41,7 +41,7 @@ class ShadowInputMethodManager2 : ShadowInputMethodManager() {
     @Implementation
     override fun getInputMethodList() = listOf(
         if (BuildConfig.BUILD_TYPE == "debug" || BuildConfig.BUILD_TYPE == "debugNoMinify")
-            InputMethodInfo("helium314.keyboard.debug", "LatinIME", "HeliBoard debug", null)
+            InputMethodInfo("helium314.keyboard.debug", "LatinIME", "NextBoard debug", null)
         else InputMethodInfo("helium314.keyboard", "LatinIME", "HeliBoard", null),
     )
     @Implementation
